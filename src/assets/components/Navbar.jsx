109697a7728ce8aa60bdad0a41ba24439fb45be2
AnubhavRishav <rishavanubhav96@gmail.com>
2024-04-18 00:09:46 +0530

    1st commit

---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React,{useState,useEffect} from 'react';
+import {MdLightMode,MdDarkMode} from "react-icons/md"
+import './navbar.css'
+import 'animate.css';
+export default function(){
+    const [darkMode,setDarkMode]= useState(false)
+    return(
+        <div className="navbar">
+            <img className="logo animate__animated animate__rubberBand animate__slow" src="./images/logo2.png" />
+            <div className="typewriter">
+                <h1>Melo<span className="headerpart">Flow</span></h1>
+            </div>
+            {!(darkMode) ? <MdLightMode className='darkmode' /> : <MdDarkMode className="button"/>}
+        </div>
+    )
+}
